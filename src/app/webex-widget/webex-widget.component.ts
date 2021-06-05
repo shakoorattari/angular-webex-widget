e85d3b6dc09a31a3481d5be210a209380feadd1e
Shakoor Hussain Attari <binmushtaq@gmail.com>
2021-06-05 23:08:08 +0400

    working test

--- src/app/webex-widget/webex-widget.component.ts
+++ src/app/webex-widget/webex-widget.component.ts
@@ -18,8 +18,8 @@ export class WebexWidgetComponent implements OnInit {
       var widgetEl = document.getElementById('my-webex-widget');
 
       webex.widget(widgetEl).spaceWidget({
-        accessToken: 'ZWFhYzk1ZDAtZWNlZS00MTVhLWJmZjMtY2MxY2I5YWYwMTAwZTMyMzIwODItZmFj_PF84_3a3c279d-f61d-4690-9800-4020b8fd7511',
-        destinationId: 'abuzzi@cisco.com',
+        guestToken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6InNoYWtvb3IiLCJzdWIiOiJzaGFrb29yIiwibmFtZSI6InNoYWtvb3IgdGVzdGluZyBhcCIsIm5iZiI6MTYyMjkwMjQyNSwiZXhwIjoxNjIzMTYxNjI1LCJpYXQiOjE2MjI5MDI0MjUsImlzcyI6IlkybHpZMjl6Y0dGeWF6b3ZMM1Z5YmpwVVJVRk5PbVYxTFdObGJuUnlZV3d0TVY5ckwwOVNSMEZPU1ZwQlZFbFBUaTh4TVRrMk9XVm1NUzFtWTJaa0xUUTRNV0V0WW1ZeVppMDRPVE5pTlRkak1EYzJNak0iLCJhdWQiOiJsbHZ1ZGZ2a3d2ZXB3a2Ruc253bXV1bHl2dHJhd3BwZiJ9._LRfWjHgwRx8gfk2PgPSEiREHMcB61AhgCH9qCW1rPM',
+        destinationId: 'shakoor.hussain@deg.shj.ae',
         destinationType: 'email',
         spaceActivities: {"files":true,"meet":true,"message":true,"people":true},
         initialActivity: 'message',
